7b9df87e2942ef03a8d183fb868a9472eb4e0a7c
Oziel Sousa <ozielzin358@gmail.com>
2023-09-07 18:10:28 -0300

    Ajustes Finais

--- src/script.js
+++ src/script.js
@@ -5,13 +5,19 @@ class Produto {
     constructor() {
         this.id = 1;
         this.arrayProdutos = [];
+        this.editId = null;
     }
 
     salvar() {
         let produto = this.lerDados();
 
         if (this.validarCampos(produto)) {
-            this.adicionar(produto)
+            if (this.editId == null) {
+            this.adicionar(produto);
+            } else{
+                this.atualizar(this.editId, produto);
+            }
+
 
         }
 
@@ -34,15 +40,17 @@ class Produto {
 
             td_id.innerText = this.arrayProdutos[i].id;
             td_produto.innerText = this.arrayProdutos[i].nomeProduto;
-            td_valor.innerText = this.arrayProdutos[i].valorProduto;
+            td_valor.innerText ="R$ " + this.arrayProdutos[i].valorProduto;
 
             td_id.classList.add('center');
 
             let imgEditar = document.createElement('img');
-            imgEditar.src = 'assets/edit.png';
+            imgEditar.src = './assets/editar.svg';
+            imgEditar.setAttribute("onclick", "produto.edicao(" + JSON.stringify(this.arrayProdutos[i]) + ")");
+            imgEditar.style.cursor = 'pointer';
 
             let imgDeletar = document.createElement('img');
-            imgDeletar.src = 'assets/delete.png';
+            imgDeletar.src = './assets/excluir-black.svg';
             imgDeletar.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id + ")");
             imgDeletar.style.cursor = 'pointer';
 
@@ -55,6 +63,33 @@ class Produto {
     adicionar(produto) {
         this.arrayProdutos.push(produto);
         this.id++;
+
+        Notification.requestPermission().then(function(permission) {
+            if (permission === "granted") {
+                var notification = new Notification("Produto cadastrado com Sucesso!", {
+                    body: "Sucesso! Seu produto foi adicionado a lista de cadastrados!",
+                    icon: "./assets/check-fat.svg"
+                });
+            }
+        });
+    }
+
+    atualizar(id, produto){
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+                this.arrayProdutos[i].valorProduto = produto.valorProduto;
+            }
+        }
+    }
+
+    edicao(dados) {
+    this.editId = dados.id;
+
+        document.getElementById('produto').value = dados.nomeProduto;
+        document.getElementById('valorProduto').value = dados.valorProduto;
+
+        document.getElementById('btnAdd').innerText = "Atualizar";
     }
 
     lerDados() {
@@ -87,20 +122,34 @@ class Produto {
     limpar() {
         document.getElementById('produto').value = '';
         document.getElementById('valorProduto').value = '';
+
+        document.getElementById('btnAdd').innerText = 'Salvar'
+        this.editId = null
     }
 
     deletar(id) {
-        if(confirm('Deseja deletar o produto do ID ' + id)){
+        if (confirm('Deseja deletar o produto do ID ' + id + " ?")) {
             let tbody = document.getElementById('tbody');
 
             for (let i = 0; i < this.arrayProdutos.length; i++) {
-                if(this.arrayProdutos[i].id == id) {
+                if (this.arrayProdutos[i].id == id) {
                     this.arrayProdutos.splice(i, 1)
                     tbody.deleteRow(i)
                 }
             }
+
+            Notification.requestPermission().then(function(permission) {
+                if (permission === "granted") {
+                    var notification = new Notification("Produto deletado com Sucesso!", {
+                        body: "Sucesso! Seu produto foi deletado da lista de cadastrados!",
+                        icon: "./assets/excluir.svg"
+                    });
+                }
+            });
         }
+
         
+
     }
 
 }
